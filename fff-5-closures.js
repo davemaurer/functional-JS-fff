87bd9440417370e555635062f9d21ef2436dd9f9
David Maurer <davemaurermail@gmail.com>
2017-04-24 15:23:46 -0600

    finish fff 5 closures

--- fff-5-closures.js
+++ fff-5-closures.js
@@ -9,4 +9,23 @@ greetMe();
 // me can be used inside of the greetMe function because of closure. It gives functions access to outer
 // variable scope.
 
+me = 'Maurer';
+
+greetMe();
+
+// use case example.
+
+function sendRequest() {
+  var requestID = '123';
+  $.ajax({
+    url: '/myUrl',
+    success: function(response) {
+      alert('Request' + requestID + 'returned')
+    }
+  });
+}
+
+// above when the call successfully completes, the callback has access to the requestID even though that variable
+// was declared outside the function.
+
 
